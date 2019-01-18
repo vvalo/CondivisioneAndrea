a9c41ae358ac87e036249e3013dc5c12073c93d7
valentino_vetro <valentino.vetro@gmail.com>
2019-01-18 14:40:15 +0100

    Primo commit

---
+++ CondivisioneConAndrea/CondivisioneConAndrea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondivisioneConAndrea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ciao Andrea!");
+            Console.ReadLine();
+        }
+    }
+}
